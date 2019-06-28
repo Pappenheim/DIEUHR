b551c5ac2de357f9c43655286acca15683282a27
Ubuntu64 <georg.graf.zu.pappenheim@stud.fh-campuswien.ac.at>
2019-06-28 14:07:50 -0700

    added comments...

--- dieuhr.sh
+++ dieuhr.sh
@@ -2,17 +2,19 @@
 
 for i in {1..999}
 do
+	#finding the zonefiles and picking a rnd one
 	TZ=$(find /usr/share/zoneinfo/ -maxdepth 1 -type f | xargs file | awk -v FS='[/:]' '/timezone data, version 2/ {print $5}' | shuf -n 1)
+	#printing time
 	clear;
 	figlet CWIX 2020;
 	figlet $(TZ=$TZ date +'%T');
+	##Exclude factory timezone
 	if [ $(TZ=$TZ date +'%Z') != "-00" ];
 	then
-		#echo $TZ;
-		#echo $(TZ=$TZ date +'%Z');
 		figlet $(TZ=$TZ date +'%Z');
 	else
 		figlet $TZ;
 	fi
+	#wait until it is necessary to update clock
 	sleep $(random -e 6 ; echo $(($? + 1)) )
 done
