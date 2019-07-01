9cce0c877ba47adc756a1e48567d83c6331bbac9
Ubuntu64 <georg.graf.zu.pappenheim@stud.fh-campuswien.ac.at>
2019-06-30 17:22:39 -0700

    more cleaning up

--- dieuhr.sh
+++ dieuhr.sh
@@ -6,10 +6,10 @@ usage() {
 	echo "Usage: $0 [-z <string>] [-p <string>]" 1>&2;
 	echo "-z	define a timezone file path from /usr/share/zoneinfo/ to use.";
 	echo "	Default is UTC"
-	echo "	see man date +%TZ for further Details";
+	echo "	see man date +'%Z' for further Details";
 	echo "";
 	echo "-m	define a header message string as \"<Content of String>\"";
-	echo "Default is \"Die Uhr\""
+	echo "	Default is \"Die Uhr\""
 	exit 1;
 	}
 
@@ -57,5 +57,6 @@ do
 	fi
 	#echo $TZ;
 	#wait until it is necessary to update clock
-	sleep $(random -e 6 ; echo $(($? + 1)) )
+	#sleep $(random -e 6 ; echo $(($? + 1)) )
+	sleep $(( $RANDOM % 7 +1));
 done
